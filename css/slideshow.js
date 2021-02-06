2d28a53dab7c869343507a727b6ebd4bea496a41
Tony Chen <tonychen820@gmail.com>
2021-02-06 01:40:34 -0500

    slideshowupdate to allow two+ slideshows

--- css/slideshow.js
+++ css/slideshow.js
@@ -1,24 +1,35 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideshow1 = document.getElementById("slideshow1");
+slideshow1.currentSlideIndex = 1;
+showSlides(slideshow1.currentSlideIndex, slideshow1);
 
-function plusSlides(n)
+
+
+var slideshow2 = document.getElementById("slideshow2");
+slideshow2.currentSlideIndex = 1;
+showSlides(slideshow2.currentSlideIndex, slideshow2);
+
+
+
+
+function plusSlides(n, slideshow)
 {
-  showSlides(slideIndex += n);
+  showSlides(slideshow.currentSlideIndex += n, slideshow);
 }
 
-function currentSlide(n)
+function currentSlide(n, slideshow)
 {
-  showSlides(slideIndex = n);
+  showSlides(slideshow.currentSlideIndex = n, slideshow);
 }
 
-function showSlides(n)
+function showSlides(n, slideshow)
 {
   var i;
-  var slides = document.getElementsByClassName("slideshow");
-  var dots = document.getElementsByClassName("slideshowthumb");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = slideshow.getElementsByClassName("slideshow");
+  var dots = slideshow.getElementsByClassName("slideshowthumb");
+  var captionText = slideshow.getElementsByClassName("caption");
+  
+  if (n > slides.length) {slideshow.currentSlideIndex = 1}
+  if (n < 1) {slideshow.currentSlideIndex = slides.length}
 
   for (i = 0; i < slides.length; i++)
   {
@@ -29,7 +40,11 @@ function showSlides(n)
   {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideshow.currentSlideIndex-1].style.display = "block";
+  dots[slideshow.currentSlideIndex-1].className += " active";
+
+  for(i = 0; i < captionText.length; i++)
+  {
+    captionText[i].innerHTML = dots[slideshow.currentSlideIndex-1].alt;
+  }
 }
